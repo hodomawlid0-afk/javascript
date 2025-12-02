c85d7261774a9e452a9b3beebc4deb037d342c5c
hotho <hodomawlid0@gmail.com>
2025-12-02 20:56:35 +0300

    first commit

---
+++ ch1_intro/script.js
@@ -0,0 +1,9 @@
+var firstname="hodo";
+var salary = 200
+var tax = 10
+var takehome = salary - tax;
+console.log(firstname, salary, tax, takehome)
+
+var lastname = "mawlid";
+var total = firstname * lastname;
+ console.log(total);
---
+++ ch2_variables_datatypes/script.js
@@ -0,0 +1,14 @@
+var firstname="hodo";
+var salary = 200
+var ismarried = false
+// var tax = 10
+// var takehome = salary - tax;
+// console.log(firstname, salary, tax, takehome)
+
+// var lastname = "mawlid";
+// var total = firstname * lastname;
+// console.log(total);
+console.log(typeof firstname);
+console.log(typeof salry);
+console.log(typeof ismarried);
+
