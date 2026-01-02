8a4b3eec3a9e866eabe081d1e590e7bec08cb6a0
hotho <hodomawlid0@gmail.com>
2026-01-02 23:45:51 +0300

    string

---
+++ ch8_string/script.js
@@ -0,0 +1,14 @@
+// let address = "#5, london road"
+// console.log(address.toUpperCase());
+// console.log(address.toLowerCase());
+// console.log(address.replace("london", "kismayo"));
+// console.log(address.indexOf("n"));
+// console.log(address.charAt(6))
+
+let email = "aligmail.com";
+if (email.indexOf("@") == -1) {
+    console.log(" the @ is missing")
+} else
+    if (email.indexOf(".") == -1) {
+        console.log(" the . is missing");
+    }
