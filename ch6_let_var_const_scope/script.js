8bb1b91dd38a0f9e57c9be16292791df7026a9cc
hotho <hodomawlid0@gmail.com>
2026-01-02 21:15:06 +0300

    var_let_const_scope

---
+++ ch6_let_var_const_scope/script.js
@@ -0,0 +1,13 @@
+{
+    //var firstname = "hodo";
+    let firstname = "hodo";
+    //const firstname = "hodo";
+
+    {
+        let lastname = "mahamed";
+        console.log(lastname)
+    }
+
+    firstname = "rahma";
+    console.log(lastnametname);
+}
