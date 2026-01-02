4d76e09ab54c10042770146f8355bb166262ccfb
hotho <hodomawlid0@gmail.com>
2026-01-02 23:08:43 +0300

    conditions_statement

---
+++ ch7_conditional_statements/script.js
@@ -0,0 +1,46 @@
+// let age = 9;
+// if (age == 5) {
+//     console.log("hello");
+//     console.log("I");
+//     console.log("am");
+//     console.log("hodo");
+//     console.log("i am 5 years old");
+// } else
+//     if (age == 6) {
+//  console.log("i am 6 years old");
+//     } else
+//         if (age == 7) {
+//  console.log("i am 7 years old");
+// } else
+// if (age == 8) {
+//  console.log("i am 8 years old");
+// }else {
+//  console.log("i am neither 5,6,7 or 8");
+// }
+
+let age = 5;
+
+switch (age) {
+  case 5:
+    console.log("hello");
+    console.log("I");
+    console.log("am");
+    console.log("hodo");
+    console.log("i am 5 years old");
+    break;
+
+  case 6:
+    console.log("i am 6 years old");
+    break;
+
+  case 7:
+    console.log("i am 7 years old");
+    break;
+
+  case 8:
+    console.log("i am 8 years old");
+    break;
+
+  default:
+    console.log("i am neither 5, 6, 7 or 8");
+}
